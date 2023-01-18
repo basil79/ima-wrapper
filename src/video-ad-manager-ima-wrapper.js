6b4f2a6756c67b2b2383c2828607934e0dded47f
Basil Goldman <basil@anyclip.com>
2023-01-18 15:51:38 +0200

    added ads-manager, check id vast url contains 'pubads.g.doubleclick.net' then use IMA

--- src/video-ad-manager-ima-wrapper.js
+++ src/video-ad-manager-ima-wrapper.js
@@ -1,8 +1,12 @@
+import {loadScript} from './utils';
+import AdError from './ad-error';
+import {AdsManager} from 'ads-manager';
+
 const VideoAdManagerIMAWrapper = function(adContainer, videoElement) {
 
   if(!(adContainer instanceof Element || adContainer instanceof HTMLDocument)
     || !(videoElement instanceof Element || videoElement instanceof HTMLDocument)) {
-    throw new Error('ad container and/or video element are not defined');
+    throw new Error('ad container and/or video element not defined');
   }
 
   this._adContainer = adContainer;
@@ -15,29 +19,42 @@ const VideoAdManagerIMAWrapper = function(adContainer, videoElement) {
     volume: 0,
     version: '!!#Version#!!'
   };
+
   // Events
+  this.EVENTS = {
+    AdsManagerLoaded: 'AdsManagerLoaded', // After success ad request, when vast xml is parsed and ready
+    AdStarted: 'AdStarted',
+    AdStopped: 'AdStopped',
+    AdSkipped: 'AdSkipped',
+    AdLoaded: 'AdLoaded',
+    AdLinearChange: 'AdLinearChange',
+    AdSizeChange: 'AdSizeChange',
+    AdExpandedChange: 'AdExpandedChange',
+    AdSkippableStateChange: 'AdSkippableStateChange',
+    AdDurationChange: 'AdDurationChange',
+    AdRemainingTimeChange: 'AdRemainingTimeChange',
+    AdVolumeChange: 'AdVolumeChange',
+    AdImpression: 'AdImpression',
+    AdClickThru: 'AdClickThru',
+    AdInteraction: 'AdInteraction',
+    AdVideoStart: 'AdVideoStart',
+    AdVideoFirstQuartile: 'AdVideoFirstQuartile',
+    AdVideoMidpoint: 'AdVideoMidpoint',
+    AdVideoThirdQuartile: 'AdVideoThirdQuartile',
+    AdVideoComplete: 'AdVideoComplete',
+    AdUserAcceptInvitation: 'AdUserAcceptInvitation',
+    AdUserMinimize: 'AdUserMinimize',
+    AdUserClose: 'AdUserClose',
+    AdPaused: 'AdPaused',
+    AdPlaying: 'AdPlaying',
+    AdError: 'AdError',
+    AdLog: 'AdLog',
+    AllAdsCompleted: 'AllAdsCompleted' // After all ads completed, vast, vpaid, vmap
+  };
   this._eventCallbacks = {};
 
   // IMA SDK ima3.js
-  this.IMA_SDK_URL = 'https://imasdk.googleapis.com/js/sdkloader/ima3.js';
-
-  this._adsManager = null; // TODO
-  this._imaManager = null; // TODO
-
-
-  this.initAdsManager();
-  this.initIMAManager();
-
-};
-VideoAdManagerIMAWrapper.prototype.init = function(width, height, viewMode) {
-  console.log('init');
-};
-// TODO:
-VideoAdManagerIMAWrapper.prototype.initAdsManager = function() {
-  console.log('init ads manager');
-};
-VideoAdManagerIMAWrapper.prototype.initIMAManager = function() {
-  console.log('init ima');
+  this.IMA_SDK_SRC = 'https://imasdk.googleapis.com/js/sdkloader/ima3.js';
   // Check that Client Side IMA SDK has been included
   // NOTE: (window['google'] && google.ima) check for any
   // IMA SDK, including SDK for Server Side ads.
@@ -46,17 +63,49 @@ VideoAdManagerIMAWrapper.prototype.initIMAManager = function() {
   // but not Server Side SDK.
   if (!window['google'] || !google.ima || !google.ima.AdsLoader) {
     console.log('ima3.js is not included');
+    loadScript(this.IMA_SDK_SRC, true, (isIMALoaded) => {
+      if(isIMALoaded) {
+        console.log('ima3.js is loaded, setup IMA');
+        // TODO: setup
+      } else {
+        //this.onAdError('IMA SDK is not loaded');
+        console.log('ima3.js is not loaded');
+      }
+    });
   } else {
     console.log('ima3.js is included, use it');
+    // TODO: setup
   }
-};
 
+  this._adsManager = new AdsManager(this._adContainer);
+  console.log('AdsManager version is', this._adsManager.getVersion());
+  this._imaManager = null; // TODO
+
+  this._useIMA = false;
+
+};
+VideoAdManagerIMAWrapper.prototype.init = function(width, height, viewMode) {
+  console.log('init');
+};
 VideoAdManagerIMAWrapper.prototype.requestAds = function(vastUrl, options = {}) {
   console.log('request ads', vastUrl);
+  // Check if vast url contains pubads.g.doubleclick.net
+  vastUrl.indexOf('pubads.g.doubleclick.net') !== -1 ? this._useIMA = true : this._useIMA = false;
+
+  console.log('use IMA', this._useIMA);
 };
 VideoAdManagerIMAWrapper.prototype.resize = function(width, height, viewMode) {
   // TODO:
 };
+// Events
+VideoAdManagerIMAWrapper.prototype.onAdError = function(message) {
+  if (this.EVENTS.AdError in this._eventCallbacks) {
+    if(typeof this._eventCallbacks[this.EVENTS.AdError] === 'function') {
+      this._eventCallbacks[this.EVENTS.AdError](typeof message !== 'object' ? new AdError(message) : message);
+    }
+  }
+};
+
 VideoAdManagerIMAWrapper.prototype.addEventListener = function(eventName, callback, context) {
   const givenCallback = callback.bind(context);
   this._eventCallbacks[eventName] = givenCallback;
